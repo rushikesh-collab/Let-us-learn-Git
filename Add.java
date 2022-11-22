e9df4ae6f079f61a904804544dbb685bc19269bd
Rushikesh Ranganath Khandagale <khandagalerushikesh98@gmail.com>
2022-11-22 12:32:16 +0530

    added message in Add.java

--- Add.java
+++ Add.java
@@ -5,5 +5,6 @@ public class Add {
 	public static void main(String[] args) {
 		int a = 3 , b = 3 ;
 		System.out.println("Addition of a and b is "+(a+b));
+		System.out.println("Add changes at 12:24");
 	}
 }
